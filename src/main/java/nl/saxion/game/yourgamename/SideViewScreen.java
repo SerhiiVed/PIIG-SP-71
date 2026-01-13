b46a74fd1770a7297815e41c6787e971295a5b5a
ChanJune <ckswns0609@gmail.com>
2026-01-13 12:19:38 +0100

    item effect and music

--- src/main/java/nl/saxion/game/yourgamename/SideViewScreen.java
+++ src/main/java/nl/saxion/game/yourgamename/SideViewScreen.java
@@ -1,6 +1,8 @@
 package nl.saxion.game.yourgamename;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Color;
 import nl.saxion.gameapp.GameApp;
@@ -39,6 +41,8 @@ public class SideViewScreen extends ScalableGameScreen {
     float bgHeight;
     float bgScale;
     private static final String MUSIC_NAME = "game1_theme_music";
+    private static final String HYPER_SOUND = "hyper_sound";
+    private static final String VIRUS_SOUND = "virus_sound";
     float endCutscene = 0;
 
     @Override
@@ -65,6 +69,8 @@ public class SideViewScreen extends ScalableGameScreen {
         GameApp.addFont("basicFont", "fonts/basic.ttf", 60);
         GameApp.addMusic(MUSIC_NAME, "audio/game1_theme.mp3");
         GameApp.playMusic(MUSIC_NAME, true, 0.5f);
+        GameApp.addSound(HYPER_SOUND, "audio/item_hyper_drive_sound.mp3");
+        GameApp.addSound(VIRUS_SOUND, "audio/item_virus_sound.mp3");
 
          bgWidth = GameApp.getTextureWidth("EuropeBG");
          bgHeight = GameApp.getTextureHeight("EuropeBG");
@@ -171,133 +177,98 @@ public class SideViewScreen extends ScalableGameScreen {
         player.x = GameApp.clamp(player.x, 0, 10000);
         GameApp.clearScreen();
 
-//          Player movement
         if (isHyperDriving) {
-            hyperDriveTimer -= delta; // Timer Decreased for delta time
+            hyperDriveTimer -= delta;
             if (hyperDriveTimer <= 0) {
-                isHyperDriving = false; // Boost done when the time is done
+                isHyperDriving = false;
                 hyperDriveTimer = 0;
             }
         }
-
         if (isviruson) {
-            virusTimer -= delta; // Timer Decreased for delta time
+            virusTimer -= delta;
             if (virusTimer <= 0) {
-                isviruson = false; // Boost done when the time is done
+                isviruson = false;
                 virusTimer = 0;
             }
         }
 
         float currentSpeed = PLAYER_SPEED;
         float jumpImpulse = 800;
+        if (isHyperDriving) { currentSpeed *= SPEED_MULTIPLIER; jumpImpulse *= JUMP_MULTIPLIER; }
+        if (isviruson) { currentSpeed *= SPEED_DIVIDER; jumpImpulse *= JUMP_DIVIDER; }
 
-        if (isHyperDriving) {
-            currentSpeed *= SPEED_MULTIPLIER;
-            jumpImpulse *= JUMP_MULTIPLIER;
-        }
-
-        if (isviruson) {
-            currentSpeed *= SPEED_DIVIDER;
-            jumpImpulse *= JUMP_DIVIDER;
-        }
-
-        if (GameApp.isKeyPressed(Input.Keys.A)) {
-            player.x -= currentSpeed * delta;
-        } if (GameApp.isKeyPressed(Input.Keys.D)) {
-            player.x += currentSpeed * delta;
-        } if (GameApp.isKeyPressed(Input.Keys.SPACE) && player.isOnGround) {
+        if (GameApp.isKeyPressed(Input.Keys.A)) player.x -= currentSpeed * delta;
+        if (GameApp.isKeyPressed(Input.Keys.D)) player.x += currentSpeed * delta;
+        if (GameApp.isKeyPressed(Input.Keys.SPACE) && player.isOnGround) {
             player.velocityY = jumpImpulse;
             player.isOnGround = false;
-        } if (player.velocityY < 0) {
-            player.isOnGround = false;
         }
-//        platforms movement
+        if (player.velocityY < 0) player.isOnGround = false;
+
         movePlatform(movingObstacle, movingObstacle.maxX, movingObstacle.minX);
         movePlatform(movingObstacle1, movingObstacle1.maxX, movingObstacle1.minX);
 
-//        Camera (follow player)
         if (player.x > camera.cameraX + camera.cameraTriggerF) {
             camera.cameraX = player.x - camera.cameraTriggerF;
         } else if (player.x < camera.cameraX + camera.cameraTriggerB) {
             camera.cameraX = player.x - camera.cameraTriggerB;
         }
-        player.velocityY = player.velocityY - (gravity * delta);
-        player.y += player.velocityY*delta;
-        if (player.y <= 0) {
-            player.y = 0;
-            player.isOnGround = true;
-        }
 
-//        Collecting items
+        player.velocityY = player.velocityY - (gravity * delta);
+        player.y += player.velocityY * delta;
+        if (player.y <= 0) { player.y = 0; player.isOnGround = true; }
+
         for (Item item : worldItems) {
-            if (!item.collected && GameApp.rectOverlap( player.x, player.y, PLAYER_SIZE, PLAYER_SIZE, item.x, item.y, ITEM_SIZE, ITEM_SIZE)) {
+            if (!item.collected && GameApp.rectOverlap(player.x, player.y, PLAYER_SIZE, PLAYER_SIZE, item.x, item.y, ITEM_SIZE, ITEM_SIZE)) {
                 collectItem(item);
             }
         }
-//        Adding relation of player to obstacle
-        for (Obstacle ob: obstacles) {
+        for (Obstacle ob : obstacles) {
             ob.playerMovement(oldPosY, oldPosX);
             movingObstacle.playerMovement(oldPosY, oldPosX);
             movingObstacle1.playerMovement(oldPosY, oldPosX);
         }
 
-        float drawWidth = bgWidth * bgScale;
-        float drawHeight = bgHeight * bgScale;
-
-        if (player.x >= 8300.0 && endCutscene == 0) {
-            endCutscene = 1;
-        }
-
-
+        if (player.x >= 8300.0 && endCutscene == 0) endCutscene = 1;
 
         GameApp.startSpriteRendering();
-            GameApp.drawTexture("EuropeBG", -300-camera.cameraX, 0, drawWidth,  drawHeight);
+        float drawWidth = bgWidth * bgScale;
+        float drawHeight = bgHeight * bgScale;
+        GameApp.drawTexture("EuropeBG", -300 - camera.cameraX, 0, drawWidth, drawHeight);
 
-                for (Obstacle ob: obstacles) {
-                    if (ob.texture.equals("cargo")){
-                        GameApp.drawTexture(ob.texture, ob.x - camera.cameraX, ob.y, ob.w, 600);
-                    } else {
-                        GameApp.drawTexture(ob.texture, ob.x - camera.cameraX, ob.y, ob.w, ob.h);
-                    }
-                }
-            GameApp.drawTexture(movingObstacle.texture ,movingObstacle.x - camera.cameraX, movingObstacle.y, movingObstacle.w, 600);
-        GameApp.drawTexture(movingObstacle1.texture,movingObstacle1.x - camera.cameraX, movingObstacle1.y, movingObstacle1.w, 600);
-            GameApp.drawAnimation("characterWalk", player.x - camera.cameraX, player.y, PLAYER_SIZE, PLAYER_SIZE);
+        for (Obstacle ob : obstacles) {
+            GameApp.drawTexture(ob.texture, ob.x - camera.cameraX, ob.y, ob.w, ob.texture.equals("cargo") ? 600 : ob.h);
+        }
+        GameApp.drawTexture(movingObstacle.texture, movingObstacle.x - camera.cameraX, movingObstacle.y, movingObstacle.w, 600);
+        GameApp.drawTexture(movingObstacle1.texture, movingObstacle1.x - camera.cameraX, movingObstacle1.y, movingObstacle1.w, 600);
+        GameApp.drawAnimation("characterWalk", player.x - camera.cameraX, player.y, PLAYER_SIZE, PLAYER_SIZE);
 
         for (Item item : worldItems) {
             if (!item.collected) {
-
-                String textureId = null;
-
-                if (item.name.equals("Hyper-drive")) {
-                    textureId = "Hyper-drive";
-                }
-                else if (item.name.equals("Virus")) {
-                    textureId = "Virus";
-                }
-                if (textureId != null) {
-                    GameApp.drawTexture(
-                            textureId,
-                            item.x - camera.cameraX,
-                            item.y,
-                            ITEM_SIZE,
-                            ITEM_SIZE
-                    );
-                }
+                GameApp.drawTexture(item.name, item.x - camera.cameraX, item.y, ITEM_SIZE, ITEM_SIZE);
             }
         }
-            int y = 600;
-            GameApp.drawText("tech100", "Inventory", 50, y, Color.WHITE);
-            y -= 50;
 
+        int invY = 600;
+        GameApp.drawText("tech100", "Inventory", 50, invY, Color.WHITE);
         for (Item i : inventory) {
-            GameApp.drawText("tech50", i.name + " x" + i.amount, 50, y, Color.WHITE);
-            y -= 35;
+            invY -= 35;
+            GameApp.drawText("tech50", i.name + " x" + i.amount, 50, invY, Color.WHITE);
+        }
+        GameApp.endSpriteRendering();
+        GameApp.startShapeRenderingFilled();
+
+        Gdx.gl.glEnable(GL20.GL_BLEND);
+        Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
+
+        if (isHyperDriving) {
+            GameApp.drawRect(0, 0, getWorldWidth(), getWorldHeight(), new Color(0.6f, 0f, 1f, 0.1f));
+            drawGlitches();
+        } else if (isviruson) {
+            GameApp.drawRect(0, 0, getWorldWidth(), getWorldHeight(), new Color(0f, 1f, 0f, 0.1f));
+            drawGlitches();
         }
 
-        GameApp.endSpriteRendering();
-
-        GameApp.startShapeRenderingFilled();
         if (endCutscene >= 1) {
             endCutscene += 7;
             GameApp.drawRectCentered(getWorldWidth()/2, getWorldHeight()/2, endCutscene, endCutscene, "black");
@@ -305,8 +276,23 @@ public class SideViewScreen extends ScalableGameScreen {
                 GameApp.switchScreen("TopDownScreen");
             }
         }
+
         GameApp.endShapeRendering();
 
+        Gdx.gl.glDisable(GL20.GL_BLEND);
+    }
+
+
+    private void drawGlitches() {
+        for (int i = 0; i < 5; i++) {
+            float gx = GameApp.random(0, (int)getWorldWidth());
+            float gy = GameApp.random(0, (int)getWorldHeight());
+            float gw = GameApp.random(50, 400);
+            float gh = GameApp.random(2, 10);
+
+            String glitchColor = GameApp.random(0, 1) == 0 ? "white" : "black";
+            GameApp.drawRect(gx, gy, gw, gh, glitchColor);
+        }
     }
 
     @Override
@@ -320,34 +306,36 @@ public class SideViewScreen extends ScalableGameScreen {
         GameApp.disposeTexture("EuropeBG");
         GameApp.disposeSpritesheet("characterEurope");
         GameApp.disposeAnimation("characterWalk");
+        GameApp.disposeSound(HYPER_SOUND);
+        GameApp.disposeSound(VIRUS_SOUND);
 
     }
 
 //    Custom methods
 //
-    public void collectItem(Item collectedItem) {
-        collectedItem.collected = true;
-
-        if (collectedItem.name.equals("Hyper-drive")) {
-            isHyperDriving = true;
-            hyperDriveTimer = HYPER_DRIVE_DURATION;
-        }
-        if (collectedItem.name.equals("Virus")) {
-            isviruson = true;
-            virusTimer = VIRUS_DURATION;
-        }
-        for (Item i : inventory) {
-            if (i.name.equals(collectedItem.name)) {
-                i.amount++;
-                return;
-            }
-        }
-        Item newItem = new Item();
-        newItem.name = collectedItem.name;
-        newItem.amount = 1;
-
-        inventory.add(newItem);
+public void collectItem(Item collectedItem) {
+    collectedItem.collected = true;
+    if (collectedItem.name.equals("Hyper-drive")) {
+        isHyperDriving = true;
+        hyperDriveTimer = HYPER_DRIVE_DURATION;
+        GameApp.playSound(HYPER_SOUND);
+    } else if (collectedItem.name.equals("Virus")) {
+        isviruson = true;
+        virusTimer = VIRUS_DURATION;
+        GameApp.playSound(VIRUS_SOUND);
     }
+    for (Item i : inventory) {
+        if (i.name.equals(collectedItem.name)) {
+            i.amount++;
+            return;
+        }
+    }
+
+    Item newItem = new Item();
+    newItem.name = collectedItem.name;
+    newItem.amount = 1;
+    inventory.add(newItem);
+}
 
 
     public void movePlatform (Obstacle ob, int x, int y) {
--- src/main/java/nl/saxion/game/yourgamename/TopDownScreen.java
+++ src/main/java/nl/saxion/game/yourgamename/TopDownScreen.java
@@ -1,6 +1,8 @@
 package nl.saxion.game.yourgamename;
 
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Color;
 import nl.saxion.gameapp.GameApp;
 import nl.saxion.gameapp.screens.ScalableGameScreen;
@@ -18,12 +20,39 @@ public class TopDownScreen extends ScalableGameScreen {
     float bossHealthBarX = 400;
     float bossHealthCurrentWidth = 300;
 
+    public static final int PLAYER_SPEED = 600;
+
+    boolean isHyperDriving = false;
+    float hyperDriveTimer = 0f;
+    final float HYPER_DRIVE_DURATION = 4.0f;
+    final float SPEED_MULTIPLIER = 1.2f;
+    final float JUMP_MULTIPLIER = 1.2f;
+
+    boolean isviruson = false;
+    float virusTimer = 0f;
+    final float VIRUS_DURATION = 4.0f;
+    final float SPEED_DIVIDER = 0.3f;
+    final float JUMP_DIVIDER = 0.3f;
+
+    public static final int ITEM_SIZE= 100;
+
+    private float itemSpawnTimer = 9.0f;
+    private float itemActiveTimer = 0f;
+    private float itemX, itemY;
+    private String currentItemOnField = "";
+    private int spawnPhase = 0;
+
     Player PlayerCharacter = new Player();
     Boss BossCharacter = new Boss();
     GlobalArrays Parts = new GlobalArrays();
     int attkCount = 1;
     float attkCooldown = 0f;
     private static final String MUSIC_NAME = "game2_theme_music";
+    private static final String HYPER_SOUND = "hyper_sound";
+    private static final String VIRUS_SOUND = "virus_sound";
+    private static final String WIN_MUSIC = "win_music";
+    private static final String LOSE_MUSIC = "lose_music";
+    private boolean isGameOverMusicPlaying = false;
     boolean cutscene = false;
     float fadeIn = 1500;
 
@@ -45,6 +74,12 @@ public class TopDownScreen extends ScalableGameScreen {
         GameApp.addFont("basic2", "fonts/basic.ttf", 200);
         GameApp.addMusic(MUSIC_NAME, "audio/game2_theme.mp3");
         GameApp.playMusic(MUSIC_NAME, true, 0.5f);
+        GameApp.addTexture("Hyper-drive", "textures/item_upgrade_hyper_drive.png");
+        GameApp.addTexture("Virus", "textures/item_downgrade_virus.png");
+        GameApp.addSound(HYPER_SOUND, "audio/item_hyper_drive_sound.mp3");
+        GameApp.addSound(VIRUS_SOUND, "audio/item_virus_sound.mp3");
+        GameApp.addMusic(WIN_MUSIC, "audio/win_music.mp3");
+        GameApp.addMusic(LOSE_MUSIC, "audio/lose_music.mp3");
 
         PlayerCharacter.TopDownWidth = 40;
         PlayerCharacter.TopDownHeight = 40;
@@ -70,6 +105,9 @@ public class TopDownScreen extends ScalableGameScreen {
 
         super.render(delta);
         GameApp.updateTimers();
+
+        handleItemSpawning(delta);
+
         //Starting cutscene
         if (!cutscene) {
             if (fadeIn > 1) {
@@ -82,6 +120,21 @@ public class TopDownScreen extends ScalableGameScreen {
             }
         }
 
+        if (isHyperDriving) {
+            hyperDriveTimer -= delta;
+            if (hyperDriveTimer <= 0) {
+                isHyperDriving = false;
+                hyperDriveTimer = 0;
+            }
+        }
+        if (isviruson) {
+            virusTimer -= delta;
+            if (virusTimer <= 0) {
+                isviruson = false;
+                virusTimer = 0;
+            }
+        }
+
 //        Health bar
         float healthSegment = healthBarsWidth / PlayerCharacter.maxHealth;
         currentHealthWidth = healthSegment * PlayerCharacter.currentHealth;
@@ -89,39 +142,51 @@ public class TopDownScreen extends ScalableGameScreen {
         float bossHealthSegment = healthBarsWidth / BossCharacter.maxHealth;
         bossHealthCurrentWidth = bossHealthSegment * BossCharacter.currentHealth;
 
+        float currentSpeed = PLAYER_SPEED;
+        float jumpImpulse = 800;
+        if (isHyperDriving) { currentSpeed *= SPEED_MULTIPLIER; jumpImpulse *= JUMP_MULTIPLIER; }
+        if (isviruson) { currentSpeed *= SPEED_DIVIDER; jumpImpulse *= JUMP_DIVIDER; }
 
         if (PlayerCharacter.currentHealth != 0f && BossCharacter.currentHealth != 0f && cutscene) {
-            float speed = 500;
 
             if (GameApp.isKeyPressed(Input.Keys.W)) {
                 if (!((PlayerCharacter.y + PlayerCharacter.TopDownHeight) >= getWorldHeight())) {
-                    PlayerCharacter.y += speed * delta;
+                    PlayerCharacter.y += currentSpeed * delta;
                 }
             }
             if (GameApp.isKeyPressed(Input.Keys.S)) {
                 if (!(PlayerCharacter.y <= 0)) {
-                    PlayerCharacter.y -= speed * delta;
+                    PlayerCharacter.y -= currentSpeed * delta;
                 }
             }
             if (GameApp.isKeyPressed(Input.Keys.A)) {
                 if (!(PlayerCharacter.x <= 0)) {
-                    PlayerCharacter.x -= speed * delta;
+                    PlayerCharacter.x -= currentSpeed * delta;
                 }
             }
             if (GameApp.isKeyPressed(Input.Keys.D)) {
                 if (!((PlayerCharacter.x + PlayerCharacter.TopDownWidth) >= getWorldWidth())) {
-                    PlayerCharacter.x += speed * delta;
+                    PlayerCharacter.x += currentSpeed * delta;
                 }
             }
 
             if (attkCooldown > 0) {
                 attkCooldown -= delta;
             }
-//            if (GameApp.isKeyPressed(Input.Keys.SPACE)) {
-                if (attkCooldown <= 0f) {
-                    BossCharacter.shootAtSelf();
-                    attkCooldown = 0.25f;   // 1 second cooldown
+            if (attkCooldown <= 0f) {
+                BossCharacter.shootAtSelf();
+
+                float baseCooldown = 0.5f;
+
+                if (isHyperDriving) {
+                    attkCooldown = baseCooldown * 0.5f;
+                } else if (isviruson) {
+                    attkCooldown = baseCooldown * 2.0f;
+                } else {
+                    attkCooldown = baseCooldown;
                 }
+            }
+
 //            }
 
             //Iterates through all damage parts and checks if the damage part has hit the player and cleans out of view parts
@@ -158,7 +223,22 @@ public class TopDownScreen extends ScalableGameScreen {
             }
         }
 
+        if (!currentItemOnField.equals("") && cutscene) {
+            if (GameApp.rectOverlap(PlayerCharacter.x, PlayerCharacter.y, PlayerCharacter.TopDownWidth, PlayerCharacter.TopDownHeight,
+                    itemX, itemY, ITEM_SIZE, ITEM_SIZE)) {
+                activateItemEffect(currentItemOnField);
 
+                if (spawnPhase == 1) {
+                    spawnPhase = 2;
+                    itemSpawnTimer = 6.0f;
+                } else if (spawnPhase == 3) {
+                    spawnPhase = 4;
+                }
+
+                currentItemOnField = "";
+                itemActiveTimer = 0;
+            }
+        }
 
         // Draw elements
         GameApp.clearScreen("black");
@@ -166,7 +246,9 @@ public class TopDownScreen extends ScalableGameScreen {
         GameApp.drawTexture("EuropeBossBG", 0, 0, GameApp.getTextureWidth("EuropeBossBG")/3, GameApp.getTextureHeight("EuropeBossBG")/3);
 //        GameApp.drawTexture("EuropeBoss", BossCharacter.x, BossCharacter.y, BossCharacter.w, BossCharacter.h);
         GameApp.drawAnimation("bossShoot",  BossCharacter.x, BossCharacter.y,  BossCharacter.w, BossCharacter.h);
-
+        if (!currentItemOnField.equals("")) {
+            GameApp.drawTexture(currentItemOnField, itemX, itemY, ITEM_SIZE, ITEM_SIZE);
+        }
 
         if (PlayerCharacter.currentHealth == 0f) {
             GameApp.drawTextCentered("tech300", "You Died :(", getWorldWidth()/2, getWorldHeight()/2, "red-600");
@@ -177,27 +259,126 @@ public class TopDownScreen extends ScalableGameScreen {
 
         }
         GameApp.endSpriteRendering();
+        GameApp.startSpriteRendering();
+
+        // (You Died)
+        if (PlayerCharacter.currentHealth == 0f) {
+            if (!isGameOverMusicPlaying) {
+                GameApp.stopMusic(MUSIC_NAME); // 배경음 멈춤
+                GameApp.playMusic(LOSE_MUSIC, false, 0.7f);
+                isGameOverMusicPlaying = true;
+            }
+            GameApp.drawTextCentered("tech300", "You Died :(", getWorldWidth()/2, getWorldHeight()/2, "red-600");
+        }
+
+        // (You Won)
+        if (BossCharacter.currentHealth == 0f) {
+            if (!isGameOverMusicPlaying) {
+                GameApp.stopMusic(MUSIC_NAME);
+                GameApp.playMusic(WIN_MUSIC, false, 0.7f);
+                isGameOverMusicPlaying = true;
+            }
+            GameApp.drawTextCentered("tech300", "!!! You Won !!!", getWorldWidth()/2, getWorldHeight()/2, "green-500");
+        }
+
+        GameApp.endSpriteRendering();
+
         GameApp.startShapeRenderingFilled();
-            GameApp.drawRect(PlayerCharacter.x, PlayerCharacter.y, PlayerCharacter.TopDownWidth, PlayerCharacter.TopDownHeight, "black");
 
-            GameApp.drawRect(healthBarX, GameApp.getWorldHeight() - 80, healthBarsWidth, healthBarsHeight, Color.RED);
-            GameApp.drawRect(healthBarX, GameApp.getWorldHeight() - 80, currentHealthWidth, healthBarsHeight, Color.GREEN);
+        Gdx.gl.glEnable(GL20.GL_BLEND);
+        Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
 
-            GameApp.drawRect(GameApp.getWorldWidth() - bossHealthBarX, GameApp.getWorldHeight() - 80, healthBarsWidth, healthBarsHeight, Color.RED);
-            GameApp.drawRect(GameApp.getWorldWidth() - bossHealthBarX, GameApp.getWorldHeight() - 80, bossHealthCurrentWidth, healthBarsHeight, Color.GREEN);
+        GameApp.drawRect(PlayerCharacter.x, PlayerCharacter.y, PlayerCharacter.TopDownWidth, PlayerCharacter.TopDownHeight, "black");
+        GameApp.drawRect(healthBarX, GameApp.getWorldHeight() - 80, healthBarsWidth, healthBarsHeight, Color.RED);
+        GameApp.drawRect(healthBarX, GameApp.getWorldHeight() - 80, currentHealthWidth, healthBarsHeight, Color.GREEN);
+        GameApp.drawRect(GameApp.getWorldWidth() - bossHealthBarX, GameApp.getWorldHeight() - 80, healthBarsWidth, healthBarsHeight, Color.RED);
+        GameApp.drawRect(GameApp.getWorldWidth() - bossHealthBarX, GameApp.getWorldHeight() - 80, bossHealthCurrentWidth, healthBarsHeight, Color.GREEN);
 
-            if (fadeIn > 1) {
-                GameApp.drawRectCentered(getWorldWidth()/2, getWorldHeight()/2, fadeIn, fadeIn, "black");
-            }
-            //draws all damage parts on the screen
-            for (Part dp : Parts.Parts) {
-                GameApp.drawRect(dp.x, dp.y, dp.w, dp.h, dp.color);
-            }
+        if (isHyperDriving) {
+            GameApp.drawRect(0, 0, getWorldWidth(), getWorldHeight(), new Color(0.6f, 0f, 1f, 0.1f));
+            drawGlitches();
+        } else if (isviruson) {
+            GameApp.drawRect(0, 0, getWorldWidth(), getWorldHeight(), new Color(0f, 1f, 0f, 0.1f));
+            drawGlitches();
+        }
+
+        if (fadeIn > 1) {
+            GameApp.drawRectCentered(getWorldWidth()/2, getWorldHeight()/2, fadeIn, fadeIn, "black");
+        }
+        for (Part dp : Parts.Parts) {
+            GameApp.drawRect(dp.x, dp.y, dp.w, dp.h, dp.color);
+        }
+
         GameApp.endShapeRendering();
+        Gdx.gl.glDisable(GL20.GL_BLEND);
+    }
+
+    private void drawGlitches() {
+        for (int i = 0; i < 5; i++) {
+            float gx = GameApp.random(0, (int)getWorldWidth());
+            float gy = GameApp.random(0, (int)getWorldHeight());
+            float gw = GameApp.random(50, 400);
+            float gh = GameApp.random(2, 10);
+
+            String glitchColor = GameApp.random(0, 1) == 0 ? "white" : "black";
+            GameApp.drawRect(gx, gy, gw, gh, glitchColor);
+        }
+    }
+
+    private void handleItemSpawning(float delta) {
+        if (!currentItemOnField.equals("")) {
+            itemActiveTimer -= delta;
+            if (itemActiveTimer <= 0) {
+                if (spawnPhase == 1) {
+                    spawnPhase = 2;
+                    itemSpawnTimer = 3.0f;
+                } else if (spawnPhase == 3) {
+                    spawnPhase = 4;
+                }
+                currentItemOnField = "";
+            }
+            return;
+        }
+
+
+        if (spawnPhase == 0 || spawnPhase == 2) {
+            itemSpawnTimer -= delta;
+            if (itemSpawnTimer <= 0) {
+                spawnPhase++;
+                currentItemOnField = (spawnPhase == 1) ? "Hyper-drive" : "Virus";
+
+                itemX = GameApp.random(100, (int)getWorldWidth() - 200);
+                itemY = GameApp.random(100, (int)getWorldHeight() - 200);
+
+                itemActiveTimer = 5.0f;
+            }
+        }
+    }
+
+    private void activateItemEffect(String itemName) {
+        if (itemName.equals("Hyper-drive")) {
+            isHyperDriving = true;
+            hyperDriveTimer = HYPER_DRIVE_DURATION;
+            GameApp.playSound(HYPER_SOUND);
+        } else if (itemName.equals("Virus")) {
+            isviruson = true;
+            virusTimer = VIRUS_DURATION;
+            GameApp.playSound(VIRUS_SOUND);
+        }
     }
 
     @Override
     public void hide() {
-
+        GameApp.stopMusic(MUSIC_NAME);
+        GameApp.disposeMusic(MUSIC_NAME);
+        GameApp.disposeSound(HYPER_SOUND);
+        GameApp.disposeSound(VIRUS_SOUND);
+        GameApp.stopMusic(WIN_MUSIC);
+        GameApp.disposeMusic(WIN_MUSIC);
+        GameApp.stopMusic(LOSE_MUSIC);
+        GameApp.disposeMusic(LOSE_MUSIC);
+        GameApp.disposeFont("tech300");
+        GameApp.disposeFont("basic");
+        GameApp.disposeFont("basic2");
     }
 }
